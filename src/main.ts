3f39e4609e17a5a4cbc24adcd87ab5120c339a55
ELOUIZ BADR <elouiz.badr@gmail.com>
2021-02-18 21:57:56 +0000

    Enable best practices: + Helmet for HTTP headers protection + CSRF protection + CORS activation + Rate limiting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // HTTP headers vulnerabilites protection
+  app.use(helmet());
+  // CSRF protection
+  app.use(csurf());
+  // CORS activation
+  app.enableCors({ origin: ['localhost'] });
+  // Rate limiting
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
