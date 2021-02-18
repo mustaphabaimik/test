8005c67caff6272c332fa79ce9ad85acba0334e5
ELOUIZ BADR <elouiz.badr@gmail.com>
2021-02-18 22:29:51 +0000

    + No need for CSRF protection (using JSON-based clients like Angular) + Setup documentation with Swagger OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import * as csurf from 'csurf';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as rateLimit from 'express-rate-limit';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
@@ -8,8 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // HTTP headers vulnerabilites protection
   app.use(helmet());
-  // CSRF protection
-  app.use(csurf());
   // CORS activation
   app.enableCors({ origin: ['localhost'] });
   // Rate limiting
@@ -19,6 +17,19 @@ async function bootstrap() {
       max: 100, // limit each IP to 100 requests per windowMs
     }),
   );
+
+  // Configure documentation with Swagger & OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Starter API')
+    .setDescription(
+      'The NestJS Starter API documentation example with Swagger & OpenAPI',
+    )
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Run the App
   await app.listen(3000);
 }
 bootstrap();
